6153d7024949ab8081954392a0ea83f23cef3e3f
HuangWei <market@zuiyue.com>
2023-08-15 16:32:18 +0800

    0.1.5

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    wei_env::bin_init("wei");
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        help();
+        std::process::exit(1);
+    }
+    let command = &args[1];
+
+    match command.as_str() {
+        "run" => {
+            // 先去当前目录bin下面找对应的exe文件，如果没有，则去wei_env::dir_bin下面找对应执行的路径
+            // 如果还是没有，则去网络上面查找有没有对应的exe文件，如果有则去下载。并提示当前正在下载文件
+            // 如果在网络上面没有找到对应的exe文件，则提示失败
+            let data = run(&args[2], std::env::args().skip(2).collect()).unwrap();
+            println!("data:[{}]", data);
+        },
+        "daemon" => {
+            println!("daemon");
+        },
+        _ => {
+            
+        }
+    }
+}
+
+fn help() {
+    let args: Vec<String> = env::args().collect();
+    eprintln!("Usage:");
+    eprintln!("  {} run <command> <param1> <param2>", args[0]);
+    eprintln!("  {} api <json>", args[0]);
+}
+
+fn run(command: &str, param: Vec<String>) -> Result<String, Box<dyn std::error::Error>> {
+    let path = std::path::Path::new("./bin/").join(command).join(".exe");
+    if path.exists() {
+        return wei_run::run(&path.display().to_string(), param);
+    }
+
+    let path = wei_env::read(&wei_env::dir_bin(),command)?;
+    wei_run::run(path.as_str(), param)
 }
