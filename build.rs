d49e54f7a099e1767aaa00d4661def6a30868b3e
HuangWei <market@zuiyue.com>
2023-12-06 17:24:54 +0800

    new logo

--- build.rs
+++ build.rs
@@ -1,7 +1,8 @@
 fn main() {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
-        res.set_icon("res/wei.ico");
+        let icon_path = std::env::var("ICON_PATH").unwrap_or_else(|_| "res/bear.ico".to_string());
+        res.set_icon(&icon_path);
         res.compile().unwrap();
     }
 }
---
+++ res/README.md
@@ -0,0 +1,5 @@
+# 工具
+
+- svg editor: https://editsvg.online/editor.html
+- svg to png: 使用 ADOBE AI
+- png to ico: https://www.aconvert.com/cn/icon/png-to-ico/
